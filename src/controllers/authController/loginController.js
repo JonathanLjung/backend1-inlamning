03b9907b70914bcf79443fb218aeedadeef6e1c1
Jonathan <jonteljungp@gmail.com>
2023-03-29 12:30:00 +0200

    Added joi validation to loginController

--- src/controllers/authController/loginController.js
+++ src/controllers/authController/loginController.js
@@ -1,8 +1,38 @@
 const bcrypt = require('bcrypt');
 const { pool } = require('../../db');
 
+const joi = require('joi');
+
+const schema = joi.object({
+  username: joi.string()
+    .min(3)
+    .max(25)
+    .required()
+    .messages({
+      "string.min": "Användarnamnet måste vara minst 3 tecken",
+      "string.max": "Användarnamnet får inte vara längre än 25 tecken",
+      "any.required": "Användarnamn är obligatoriskt",
+    }),
+  password: joi.string()
+    .min(6)
+    .max(30)
+    .required()
+    .messages({
+      "string.min": "Lösenordet måste vara minst 6 tecken",
+      "string.max": "Lösenordet får inte vara längre än 30 tecken",
+      "any.required": "Lösenord är obligatoriskt",
+    }),
+});
+
 const login = (req, res) => {
-  const { username, password } = req.body;
+  const { error, value } = schema.validate(req.body);
+
+  if (error) {
+    const errorMessage = error.details[0].message;
+    return res.status(400).json({ error: errorMessage });
+  }
+
+  const { username, password } = value;
 
   const getUserData = `
     SELECT id, password FROM users WHERE BINARY username=?`;
@@ -24,10 +54,14 @@ const login = (req, res) => {
         } else if (!isMatch) {
           res.status(401).json({ error: "Fel användarnamn eller lösenord" });
         } else {
-          res.cookie("loginCookie", JSON.stringify({ userId: userId, username }), {
-            maxAge: 20 * 60 * 1000,
-            httpOnly: true,
-          });
+          res.cookie(
+            "loginCookie",
+            JSON.stringify({ userId: userId, username }),
+            {
+              maxAge: 20 * 60 * 1000,
+              httpOnly: true,
+            }
+          );
           res.status(200).json("Du är nu inloggad");
         }
       });
