88eae4a12b409a4f341b25929f176261af8b3694
Jonathan <jonteljungp@gmail.com>
2023-03-29 12:33:55 +0200

    Added joi validation to getTodosController

--- src/controllers/todosController/getTodosController.js
+++ src/controllers/todosController/getTodosController.js
@@ -1,20 +1,33 @@
 const { pool } = require('../../db');
+const joi = require("joi");
+
+const schema = joi.object({
+  user_id: joi.number()
+  .integer()
+  .min(1)
+  .required()
+});
 
 const getTodos = (req, res) => {
-  const user_id = req.userId;
+  const { error, value } = schema.validate({ user_id: req.userId });
 
-  const getTodos = `
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const { user_id } = value;
+
+  const getTodosQuery = `
     SELECT * FROM todos WHERE user_id=?`;
 
-    if (user_id !== undefined) {
-  pool.execute(getTodos, [user_id], (error, result) => {
+  pool.execute(getTodosQuery, [user_id], (error, result) => {
     if (error) {
       console.log(error);
-      res.status(401).json("Du är inte inloggad");
+      res.status(500).json("Ett internt fel uppstod");
     } else {
       res.status(200).json(result);
     }
   });
-}};
+};
 
-module.exports = { getTodos }
+module.exports = { getTodos };
