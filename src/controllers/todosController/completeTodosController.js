9a26624d4240ca94baeb519c720e5b3be9916454
Jonathan <jonteljungp@gmail.com>
2023-03-29 12:30:34 +0200

    Added joi validation to completeTodosController

--- src/controllers/todosController/completeTodosController.js
+++ src/controllers/todosController/completeTodosController.js
@@ -1,9 +1,23 @@
 const { pool } = require('../../db');
+const joi = require('joi');
+
+const todoValidationSchema = joi.object({
+  isComplete: joi.boolean().required(),
+  id: joi.number().integer().positive().required(),
+});
+
+const validateTodo = (data) => {
+  return todoValidationSchema.validate(data);
+};
 
 const completeTodos = (req, res) => {
   const { id } = req.params;
   const { isComplete } = req.body;
 
+  const { error } = validateTodo({ id: parseInt(id), isComplete});
+  if(error) {
+    return res.status(400).json({ error: error.details[0].message })
+  }
   const updateTodoQuery = `
   UPDATE todos
   SET complete = ?
