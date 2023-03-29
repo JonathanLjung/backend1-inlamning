88bf0aaf025d2565c04b8b87b52c1fd83a7a36ac
Jonathan <jonteljungp@gmail.com>
2023-03-29 12:30:44 +0200

    Added joi validation to deleteTodosController

--- src/controllers/todosController/deleteTodosController.js
+++ src/controllers/todosController/deleteTodosController.js
@@ -1,13 +1,26 @@
 const { pool } = require('../../db');
+const joi = require('joi');
+
+const deleteTodoSchema = joi.object({
+  id: joi.number().integer().required(),
+});
 
 const deleteTodos = (req, res) => {
   const user_id = req.userId;
-  const todoId = req.params.id;
+  const { error, value } = deleteTodoSchema.validate(req.params, { abortEarly: false });
+
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const { id } = value;
 
   const deleteTodo = `
     DELETE FROM todos WHERE id = ? AND user_id = ?`;
 
-  pool.execute(deleteTodo, [todoId, user_id], (error, result) => {
+  pool.execute(deleteTodo, [id, user_id], (error, result) => {
     if (error) {
       console.log(error);
       res.sendStatus(500);
